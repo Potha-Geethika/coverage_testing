ea323ca01eb2a62928d656fd6fdf7e9b1978cf5c
Potha-Geethika <potha.geethika@walkingtree.tech>
2026-04-08 15:01:15 +0530

    added pad-service

---
+++ pad-service/src/test/java/com/carbo/pad/PadServiceApplicationTests.java
@@ -0,0 +1,15 @@
+//package com.carbo.pad;
+//
+////import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class PadServiceApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
